d8bc2eb44bd6d2f1198aa61c0377ee7625259ee4
Kaidyn <kaidyn.phelan08@gmail.com>
2022-04-13 16:11:08 +1000

    started backend and connected mongodb

---
+++ client/src/pages/Tab3.tsx
@@ -0,0 +1,20 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab3.css';
+
+const Tab3: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 3</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab3;
